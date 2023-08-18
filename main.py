03d60e3495b467dc0ff147864e6c6e5456e10bdc
Liuperson306 <129485929+Liuperson306@users.noreply.github.com>
2023-08-18 15:17:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import streamlit as st
+
 question_1 = "Comparing the two full faces (Left and Right), which one looks more realistic?"
 options_1 = ["The Left one looks more realistic", "The Right one looks more realistic"]
 question_2 = "Comparing the lips of two faces, which one is more in sync with audio?"
